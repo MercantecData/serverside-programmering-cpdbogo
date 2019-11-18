0849404cc8b68643f4ae584254e3fc9e39e92020
Casper <cpd@bang-olufsen.dk>
2019-11-18 09:02:54 +0100

    Implement mysql from intro

--- Intro/server.js
+++ Intro/server.js
@@ -1,7 +1,19 @@
 var http = require("http")
+var mysql = require("mysql")
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Password1",
+    database: "h3_db_prog"
+});
+
 var server = http.createServer(function(req,res){  
-    res.write("Request recieded")
-    res.end()
+    con.connect(function (err) { 
+        con.query("SELECT * FROM employees", function(err,data){
+            res.write(JSON.stringify(data))
+            res.end()
+        })
+     })
 })
 
 server.listen(8080)
