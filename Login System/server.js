eee1e2ce90ef0da2f3499bd63b1cdd02a0f54f61
Casper <cpd@bang-olufsen.dk>
2019-11-28 12:57:47 +0100

    wip

---
+++ Login System/server.js
@@ -0,0 +1,140 @@
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+var textBody = require('body')
+var crypto = require('crypto')
+
+http.createServer(function(req,res){
+    var url = req.url
+    var filepath = path.join(__dirname,'data', url)
+    var ext = String(path.extname(filepath)).toLowerCase();
+    var sessions = new Array()
+    var allowed = false
+
+    if (req.headers.cookie){
+        var rawcookies = req.headers.cookie.replace(' ','').split(';')
+        cookies = new Array()
+        for (let index = 0; index < rawcookies.length; index++) {
+            var tempcookie = rawcookies[index].split('=')
+            cookies[tempcookie[0]] = tempcookie[1]
+        }
+        if (cookies['SESSID']){
+            if (sessions.includes(cookies['SESSID'])){
+                allowed = true
+            }
+        }
+    }
+    if (!allowed){
+        res.writeHead(300,  {Location: "/login.html"})
+        res.end()
+    }
+
+    if (req.method == "POST") {
+        var usersraw = fs.readFileSync('./data/users.json')
+        var users = new Object(JSON.parse(usersraw))
+        textBody(req, res, function (err, body) {
+            if (err) {
+                res.statusCode = 500
+                return res.end("Error processing request")
+            }
+            params = {}
+            bodysplit = body.split('&')
+            for (var key in bodysplit){
+                tempsplit = bodysplit[key].split('=')
+                params[tempsplit[0]] = tempsplit[1]
+            }
+
+            if (req.url == '/register.html'){
+                if(!(params['usrname'] in users)){
+                    users[params['usrname']] = params['usrpass']
+                    fs.writeFile('./data/users.json',JSON.stringify(users),'utf8', function(err, result) {})
+                    res.writeHead(300,  {Location: "/login.html"})
+                    res.end()
+                }
+                else{
+                    res.writeHead(409)
+                    res.end('User already exists')
+                }
+            }
+
+            if (req.url == '/login.html'){
+                if(params['usrname'] in users){
+                    if(users[params['usrname']] == params['usrpass']){
+                    
+                    var thissess = crypto.randomBytes(16).toString('base64');
+                    sessions.push(thissess)
+                    res.setHeader('Set-Cookie','SESSID='+thissess)
+                    // res.setHeader('Set-Cookie','SESSID='+thissess+'; expires='+new Date(new Date().getTime()+600000).toUTCString())
+                    }
+                    else{
+                        res.writeHead(403)
+                        res.end("Wrong password")
+                    }
+                }else{
+                    res.writeHead(403)
+                    res.end("User doesn't exist")
+                }
+
+            }
+
+
+        })
+    }
+
+
+
+
+
+        if (url.endsWith('/')){
+            filepath = path.join(__dirname,'data/index.html')
+        }
+        
+        console.log('incoming request', url)
+        console.log(req.method)
+        
+        //https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+        var extensions = {
+            '.aac': 'audio/aac',
+            '.abw': 'application/x-abiword',
+            '.arc': 'application/x-freearc',
+            '.avi': 'video/x-msvideo',
+            '.azw': 'application/vnd',
+            '.bin': 'application/octet-stream',
+            '.bmp': 'image/bmp',
+            '.bz':  'application/x-bzip',
+            '.bz2': 'application/x-bzip2',
+            '.csh': 'application/x-csh',
+            '.html': 'text/html',
+            '.js': 'text/javascript',
+            '.css': 'text/css',
+            '.jpg': 'image/jpeg',
+            '.php': 'application/php',
+            '.png': 'image/png',
+            '.gz': 'application/gzip',
+            '.rar': 'application/x-rar-compressed',
+            '.txt': 'text/plain',
+            '.wav': 'audio/wav',
+            '.webm': 'video/webm',
+            '.xml': 'text/xml',
+            '.zip': 'application/zip'
+        }
+        var contentheader = extensions[ext] || 'text/html'
+
+        fs.readFile(filepath, function(error, data) {
+            if (error) {
+                if (error.code == 'ENOENT'){
+                    res.writeHead(404)
+                    res.end("404: File not found")
+                } 
+                else {
+                    res.writeHead(500)
+                    res.end("5xx, server done goofed")
+                }
+            }
+            else {
+                res.writeHead(200, {'Content-Type': contentheader})
+                res.end(data)
+            }
+        })
+}).listen(80)
+
